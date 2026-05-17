90e5a2145d5a83fe8f05063dd1027659bf5e2cf3
murturhan <murturhan@gmail.com>
2026-05-17 23:31:32 +0300

    Update index.ts

--- remotion/src/index.ts
+++ remotion/src/index.ts
@@ -1,4 +1,10 @@
-import { registerRoot } from "remotion";
+import { registerRoot, continueRender, delayRender } from "remotion";
+import { loadFont as loadLilitaOne } from "@remotion/google-fonts/LilitaOne";
+import { loadFont as loadFredoka } from "@remotion/google-fonts/Fredoka";
 import { RemotionRoot } from "./Root";
+
+// Google Fontları yükle - Lilita One (ana), Fredoka (fallback)
+loadLilitaOne();
+loadFredoka();
 
 registerRoot(RemotionRoot);
