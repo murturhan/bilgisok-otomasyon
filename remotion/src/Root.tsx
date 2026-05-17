73723d2dc12a674b5022686ea7ebdadc209e6811
murturhan <murturhan@gmail.com>
2026-05-17 19:19:12 +0300

    Update Root.tsx

--- remotion/src/Root.tsx
+++ remotion/src/Root.tsx
@@ -2,21 +2,26 @@ import React from "react";
 import { Composition } from "remotion";
 import { KidsQuizComposition } from "./compositions/KidsQuizComposition";
 import { quizCompositionSchema, defaultQuizProps } from "./types/schemas";
-import { totalFrames, FPS } from "./styles/theme";
+import { FPS } from "./styles/theme";
+import { outroStartFrame } from "./utils/timing";
+
+// Toplam frame helper'ı
+function calculateTotalFrames(props: typeof defaultQuizProps): number {
+  const outroFrames = Math.ceil(props.outro_audio_duration * FPS);
+  const outroStart = outroStartFrame(props.intro_audio_duration, props.questions);
+  return outroStart + outroFrames;
+}
 
 export const RemotionRoot: React.FC = () => {
-  // Total frames - default props 5 soruluk olduğu için
-  // Render sırasında --props ile gerçek soru sayısı geçilecek,
-  // calculateMetadata callback'i ile dinamik süre hesaplanacak
-  const defaultTotalFrames = totalFrames(defaultQuizProps.questions.length);
+  const defaultDuration = calculateTotalFrames(defaultQuizProps);
 
   return (
     <>
-      {/* LONG video composition (1920x1080) */}
+      {/* LONG video (1920x1080) */}
       <Composition
         id="KidsQuizLong"
         component={KidsQuizComposition}
-        durationInFrames={defaultTotalFrames}
+        durationInFrames={defaultDuration}
         fps={FPS}
         width={1920}
         height={1080}
@@ -24,16 +29,16 @@ export const RemotionRoot: React.FC = () => {
         defaultProps={defaultQuizProps}
         calculateMetadata={async ({ props }) => {
           return {
-            durationInFrames: totalFrames(props.questions.length),
+            durationInFrames: calculateTotalFrames(props),
           };
         }}
       />
 
-      {/* SHORTS composition (1080x1920) */}
+      {/* SHORTS video (1080x1920) */}
       <Composition
         id="KidsQuizShorts"
         component={KidsQuizComposition}
-        durationInFrames={defaultTotalFrames}
+        durationInFrames={defaultDuration}
         fps={FPS}
         width={1080}
         height={1920}
@@ -41,7 +46,7 @@ export const RemotionRoot: React.FC = () => {
         defaultProps={defaultQuizProps}
         calculateMetadata={async ({ props }) => {
           return {
-            durationInFrames: totalFrames(props.questions.length),
+            durationInFrames: calculateTotalFrames(props),
           };
         }}
       />
