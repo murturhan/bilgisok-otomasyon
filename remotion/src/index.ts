d663dde2fec3b7d5e4d2db7e7ed6e64202771a93
murturhan <murturhan@gmail.com>
2026-05-19 14:02:36 +0300

    Update index.ts

--- remotion/src/index.ts
+++ remotion/src/index.ts
@@ -1,4 +1,9 @@
 import { registerRoot } from "remotion";
+import { loadFont as loadLilita } from "@remotion/google-fonts/LilitaOne";
+import { loadFont as loadFredoka } from "@remotion/google-fonts/Fredoka";
 import { RemotionRoot } from "./Root";
+
+loadLilita();
+loadFredoka();
 
 registerRoot(RemotionRoot);
